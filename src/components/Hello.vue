a88a2bdbbe27bcd5084b948c4106675f86ebc4b9
drkaine <duretkevin@live.fr>
2025-03-06 08:38:11 +0000

    Add error

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="hello">
-    <p>Bonjour {{ hello.name }}!</p>
+    <p>Bonjour {{ hello.name }} !</p>
+    <input v-model="name" placeholder="Entrez un nom" />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Hello } from "@/type";
+import { reactive } from "vue";
 
-const hello: Hello = {
+const hello = reactive<Hello>({
   name: "John",
-};
+});
 </script>
 
 <style scoped>
@@ -17,5 +19,12 @@ const hello: Hello = {
   padding: 1rem;
   margin-top: 1rem;
   border-top: 1px solid var(--color-border);
+}
+
+input {
+  margin-top: 0.5rem;
+  padding: 0.5rem;
+  border: 1px solid var(--color-border);
+  border-radius: 4px;
 }
 </style>
